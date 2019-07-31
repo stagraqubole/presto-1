stagraqubole/presto

Add $isValid hidden column for full ACID Hive Tables.
Add FilterNode with $isValid=true when tables has this column

presto-hive/src/main/java/io/prestosql/plugin/hive/HiveUtil.java
@@ -46,6 +46,7 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hive.common.JavaUtils;
 import org.apache.hadoop.hive.ql.exec.Utilities;
+import org.apache.hadoop.hive.ql.io.AcidUtils;
 import org.apache.hadoop.hive.ql.io.IOConstants;
 import org.apache.hadoop.hive.ql.io.SymlinkTextInputFormat;
 import org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat;
@@ -100,6 +101,7 @@ import static com.google.common.collect.Lists.newArrayList;
 import static com.google.common.collect.Lists.transform;
 import static io.prestosql.plugin.hive.HiveColumnHandle.ColumnType.PARTITION_KEY;
 import static io.prestosql.plugin.hive.HiveColumnHandle.ColumnType.REGULAR;
+import static io.prestosql.plugin.hive.HiveColumnHandle.acidRowValidityColumnHandle;
 import static io.prestosql.plugin.hive.HiveColumnHandle.bucketColumnHandle;
 import static io.prestosql.plugin.hive.HiveColumnHandle.isBucketColumnHandle;
 import static io.prestosql.plugin.hive.HiveColumnHandle.isPathColumnHandle;
@@ -816,6 +818,11 @@ public final class HiveUtil
         columns.add(pathColumnHandle());
         if (table.getStorage().getBucketProperty().isPresent()) {
             columns.add(bucketColumnHandle());
+        }
+
+        // add isValid column for ACID tables
+        if (AcidUtils.isTransactionalTable(table.getParameters())) {
+            columns.add(acidRowValidityColumnHandle());
         }
 
         return columns.build();

stagraqubole/presto-1

presto-hive/src/main/java/io/prestosql/plugin/hive/HiveUtil.java
@@ -46,6 +46,7 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hive.common.JavaUtils;
 import org.apache.hadoop.hive.ql.exec.Utilities;
+import org.apache.hadoop.hive.ql.io.AcidUtils;
 import org.apache.hadoop.hive.ql.io.IOConstants;
 import org.apache.hadoop.hive.ql.io.SymlinkTextInputFormat;
 import org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat;
@@ -100,6 +101,7 @@ import static com.google.common.collect.Lists.newArrayList;
 import static com.google.common.collect.Lists.transform;
 import static io.prestosql.plugin.hive.HiveColumnHandle.ColumnType.PARTITION_KEY;
 import static io.prestosql.plugin.hive.HiveColumnHandle.ColumnType.REGULAR;
+import static io.prestosql.plugin.hive.HiveColumnHandle.acidRowValidityColumnHandle;
 import static io.prestosql.plugin.hive.HiveColumnHandle.bucketColumnHandle;
 import static io.prestosql.plugin.hive.HiveColumnHandle.isBucketColumnHandle;
 import static io.prestosql.plugin.hive.HiveColumnHandle.isPathColumnHandle;
@@ -816,6 +818,11 @@ public final class HiveUtil
         columns.add(pathColumnHandle());
         if (table.getStorage().getBucketProperty().isPresent()) {
             columns.add(bucketColumnHandle());
+        }
+
+        // add isValid column for ACID tables
+        if (AcidUtils.isFullAcidTable(table.getParameters())) {
+            columns.add(acidRowValidityColumnHandle());
         }
 
         return columns.build();